282f3b3376730c68f17f190a00fc3d5f26f698c3
Ryan Thompson <service@aiwebsystems.com>
2014-11-03 01:31:21 -0600

    Doc'd

--- src/RequestTag.php
+++ src/RequestTag.php
@@ -2,78 +2,109 @@
 
 use Anomaly\Streams\Platform\Addon\Tag\TagAddon;
 
+/**
+ * Class RequestTag
+ *
+ * @link          http://anomaly.is/streams-platform
+ * @author        AnomalyLabs, Inc. <hello@anomaly.is>
+ * @author        Ryan Thompson <ryan@anomaly.is>
+ * @package       Anomaly\Streams\Addon\Tag\Request
+ */
 class RequestTag extends TagAddon
 {
-    protected $slug = 'request';
 
     /**
-     * Return a segment of the request.
+     * Get the value from a URI segment.
      *
-     * @return string
+     * @param int $segment
+     * @return mixed
      */
     public function segment($segment = 1)
     {
-        return \Request::segment($this->getAttribute('segment', 0, $segment));
+        return app('request')->segment($this->getAttribute('segment', $segment));
     }
 
     /**
-     * Return the segments of the request.
+     * Get the segments as an array.
      *
-     * @return array
+     * @return mixed
      */
     public function segments()
     {
-        return \Request::segments();
+        return array_map(
+            function ($segment) {
+                return compact('segment');
+            },
+            app('request')->segments()
+        );
     }
 
     /**
-     * Return the first segment of the request.
+     * Get the value of the first segment.
      *
      * @return mixed
      */
     public function first()
     {
-        return array_shift(\Request::segments());
+        return array_shift(app('request')->segments());
     }
 
     /**
-     * Return the last segment of the request.
+     * Get the value of the last segment.
      *
      * @return mixed
      */
     public function last()
     {
-        $segments = \Request::segments();
+        $segments = app('request')->segments();
 
         return end($segments);
     }
 
     /**
-     * Return a boolean flagging the request as secure or not.
+     * Return whether the request is secure or not.
      *
-     * @return bool
+     * @return mixed
      */
     public function isSecure()
     {
-        return \Request::isSecure();
+        return app('request')->isSecure();
     }
 
     /**
-     * Request path
+     * Return the request path.
      *
-     * @return string
+     * @return mixed
      */
     public function path()
     {
-        return \Request::path();
+        return app('request')->path();
     }
 
-    public function __call($key, array $params = [])
+    /**
+     * Return the full request string.
+     *
+     * @return mixed
+     */
+    public function full()
     {
-        if (substr($key, 0, 8) == 'segment_') {
-            return $this->segment(substr($key, 8));
+        return app('request')->full();
+    }
+
+    /**
+     * Override the calls to look for segment values.
+     *
+     * @param       $method
+     * @param array $arguments
+     * @return mixed|null
+     */
+    public function __call($method, array $arguments = [])
+    {
+        if (substr($method, 0, 8) == 'segment_') {
+
+            return $this->segment(substr($method, 8));
         }
 
-        return parent::__call($key, $params);
+        return parent::__call($method, $arguments);
     }
 }
