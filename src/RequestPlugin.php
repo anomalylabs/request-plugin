bca4b4a97918b5e7326ccab1951563e319380dd3
Ryan Thompson <ryan@pyrocms.com>
2016-02-18 01:53:24 -0600

    Author

--- src/RequestPlugin.php
+++ src/RequestPlugin.php
@@ -7,9 +7,9 @@ use Twig_SimpleFunction;
 /**
  * Class RequestPlugin
  *
- * @link          http://anomaly.is/streams-platform
- * @author        AnomalyLabs, Inc. <hello@anomaly.is>
- * @author        Ryan Thompson <ryan@anomaly.is>
+ * @link          http://pyrocms.com/
+ * @author        PyroCMS, Inc. <support@pyrocms.com>
+ * @author        Ryan Thompson <ryan@pyrocms.com>
  * @package       Anomaly\Streams\Addon\Plugin\Request
  */
 class RequestPlugin extends Plugin
